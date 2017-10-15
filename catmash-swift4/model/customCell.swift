eb6e1ba3b1b7a5a6bb327f71f33bfd8f91cb552c
mehdi-S <meddu94@live.fr>
2017-10-15 22:09:34 +0200

    [FEATURE] add customCell model

---
+++ catmash-swift4/model/customCell.swift
@@ -0,0 +1,15 @@
+//
+//  customCell.swift
+//  catmash-swift4
+//
+//  Created by Mehdi Silini on 15/10/2017.
+//  Copyright © 2017 Mehdi Silini. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class customCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+}
