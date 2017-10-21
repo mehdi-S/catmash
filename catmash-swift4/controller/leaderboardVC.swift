d91ab7cc28a2339b970c20a14ddb7414a7a93c9d
mehdi-S <meddu94@live.fr>
2017-10-21 23:33:20 +0200

    done adding pull to refresh behavior

--- catmash-swift4/controller/leaderboardVC.swift
+++ catmash-swift4/controller/leaderboardVC.swift
@@ -14,18 +14,49 @@ import FirebaseDatabase
 class leaderboardVC: UITableViewController {
     
     @IBOutlet var customTableView: UITableView!
+    // create an instance of the UIRefreshControl class
+    private let customRefreshControl = UIRefreshControl()
     // leaderboard[X][0] is the identifier for element X
     // leaderboard[X][1] is the score for element X
     var leaderboard = [[String]]()
-    // Database reference
+    // setting up firebase database reference
     let ref = Database.database().reference()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        observeDatabase()
         // sort the new leaderboard obtained from segueWay
         leaderboard = sortLeaderboard(leaderboard: leaderboard)
+        // Add Refresh Control to Table View
+        customRefreshControl.addTarget(self, action: #selector(refreshCellData(_:)), for: .valueChanged)
+        customTableView.refreshControl = customRefreshControl
         // reload tableView with the new sorted data
         customTableView.reloadData()
+    }
+    
+    func observeDatabase() -> Void {
+        // Database leaderboard reference
+        let scoresRef = self.ref.child("leaderboard")
+        
+        scoresRef.observe(.value, with: { (snapshot) in
+            self.leaderboard = [[String]]()
+            for child in snapshot.children {
+                let snap = child as! DataSnapshot
+                let score = snap.childSnapshot(forPath: "score")
+                var row = [String]()
+                row.append(snap.key)
+                row.append("\(score.value ?? "0")")
+                self.leaderboard.append(row)
+            }
+            self.leaderboard = self.sortLeaderboard(leaderboard: self.leaderboard)
+            print(self.leaderboard)
+        })
+    }
+    
+    @objc private func refreshCellData(_ sender: Any) {
+        // reload cell content when user pullDownToRefresh
+        customTableView.reloadData()
+        customRefreshControl.endRefreshing()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -46,6 +77,6 @@ class leaderboardVC: UITableViewController {
     
     // Sort an Array[[String]] by descending order
     func sortLeaderboard(leaderboard toSort: [[String]]) -> [[String]] {
-        return leaderboard.sorted(by: { $0[1] > $1[1] })
+        return leaderboard.sorted { $0[1].compare($1[1], options: .numeric) == ComparisonResult.orderedDescending }
     }
 }
